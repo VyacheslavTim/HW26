99ea67e6f223d6a343dd7ce306332d8dca05585e
timoshenko93 <lenincommunist194@gmail.com>
2023-05-12 23:24:01 +0300

    HW26

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10
 
 COPY requirements.txt .
-RUN python3 -n pip install --no-cache -r requirements.txt
+RUN python3 -h pip install --no-cache -r requirements.txt
 
 COPY . .
 
--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     DB_NAME = os.getenv('DB_NAME')
     DB_HOST = os.getenv('DB_HOST')
     # SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_password@localhost:5436/db_name'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_password@localhost:5436/db_name'
